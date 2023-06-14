8432a5b34945c2ecfde4404b25ea2d6b830d01a2
Utkarsh <utkarsh.harsh2002@gmail.com>
2023-06-14 15:04:09 +0530

    SignUp,Login,Profile

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,11 +6,34 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
+    colors:{
+        'background':'#0B0B0B',
+        'white':'#FFFFFF',
+        'main':'#00C29',
+        'black':'#000000'
+    },
+    fontFamily:{
+        'heading':['Autom'],
+        'sub':['Fira Code'],
+        'button':['Upheaval Pro']
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        'main':'#00C29A'
       },
     },
   },
