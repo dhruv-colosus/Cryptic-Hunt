9d985691782d9b03975ae17f746eceaafb9269ce
Dhruv <dhruvdeora31@gmail.com>
2023-06-18 21:49:27 +0530

    finished landing and added 404 page

---
+++ frontend/src/app/not-found.js
@@ -0,0 +1,15 @@
+function Custom404() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-2xl font-heading font-bold ">404 | 404 | 404</h1>
+        <p className="text-s font-sub text-main">Blud is Stupid af !!</p>
+        <p className="text-s font-sub text-black">
+          Not that Stupid Maybe XD. I love Ryan Gosling tho
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Custom404;
