330fd9a5ea8112f028e660a201c94eae41e2016e
Aryan Chourey <aryanchourey4@gmail.com>
2023-06-20 06:18:01 +0530

    rulebook

--- frontend/src/app/(auth)/page.js
+++ frontend/src/app/(auth)/page.js
@@ -1,39 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import { Rulebook } from "../../components/Rulebook";
 
 export default function Home() {
-  return (
-    <div>
-      {/* body */}
+    const [rules1, setRules1] = useState(" top-[-300vh]");
+    const rulesRule = () => {
+        if (rules1 === " top-[-300vh]") {
+            setRules1(" top-[-80vh]");
+        }
+        else if (rules1 === " top-[-80vh]") {
+            setRules1(" top-[-300vh]");
+        }
+    };
+    return (
+        <>
+            <div className="h-fit sticky top-0 ">
+                {/* body */}
 
-      <div className="bg-[#OBOBOB] h-full my-[30vh] sm:mt-44 md:mt-44 lg:mt-44 xl:mt-44 transition-all duration-300">
-        <div className="flex flex-col items-center">
-          {/* main heading */}
-          <div>
-            <div>
-              <h1 className="text-[#00C29A] font-heading text-5xl sm:text-7xl md:text-7xl lg:text-7xl xl:text-8xl transition-all duration-300">
-                CRYPTIC HUNT
-              </h1>
-            </div>
-          </div>
-          {/* main heading ends */}
+                <div className="bg-[#OBOBOB] h-full py-[30vh] sm:pt-44 md:pt-44 lg:pt-44 xl:pt-44 transition-all duration-300">
+                    <div className="flex flex-col items-center">
+                        {/* main heading */}
+                        <div>
+                            <div>
+                                <h1 className="text-[#00C29A] font-heading text-5xl sm:text-7xl md:text-7xl lg:text-7xl xl:text-8xl transition-all duration-300">
+                                    CRYPTIC HUNT
+                                </h1>
+                            </div>
+                        </div>
+                        {/* main heading ends */}
 
-          {/* under heading */}
-          <div className="pb-4">
-            <div>
-              <h3
-                className="text-[#00C29A] font-montserrat tracking-[0.75rem] sm:tracking-[1.75rem] md:tracking-[1.75rem] lg:tracking-[1.75rem] xl:tracking-[1.75rem]
+                        {/* under heading */}
+                        <div className="pb-4">
+                            <div>
+                                <h3
+                                    className="text-[#00C29A] font-montserrat tracking-[0.75rem] sm:tracking-[1.75rem] md:tracking-[1.75rem] lg:tracking-[1.75rem] xl:tracking-[1.75rem]
                text-[0.4rem] sm:text-xs md:text-xs lg:text-xs xl:text-xs transition-all duration-300"
-              >
-                LET THE HUNT BEGIN!
-              </h3>
-            </div>
-          </div>
-          {/* under heading ends */}
+                                >
+                                    LET THE HUNT BEGIN!
+                                </h3>
+                            </div>
+                        </div>
+                        {/* under heading ends */}
 
-          {/* play button */}
-          <div className="bg-[#00C29A]">
-            {/* <a
+                        {/* play button */}
+                        <div className="bg-[#00C29A]">
+                            {/* <a
               href="#"
               className=" hover:bg-background hover:text-main hover:border-main"
             >
@@ -43,89 +57,89 @@ export default function Home() {
                 </h2>
               </div>
             </a> */}
-            <button
-              type="submit"
-              className="font-bold font-heading text-xl block text-center outline-none bg-main text-white cursor-pointer transition  duration-300
+                            <button
+                                type="submit"
+                                className="font-bold font-heading text-xl block text-center outline-none bg-main text-white cursor-pointer transition  duration-300
                 sm:py-2 py-1 sm:px-12 px-10 border-2 border-main hover:bg-background hover:text-main
                  onClick={login}"
-            >
-              Play
-            </button>
-          </div>
-          {/* play button ends */}
-        </div>
-      </div>
-      {/* body ends */}
+                            >
+                                Play
+                            </button>
+                        </div>
+                        {/* play button ends */}
+                    </div>
+                </div>
+                {/* body ends */}
 
-      {/* side buttons */}
-      <div className="mt-[6vw] mx-4 mb-4 absolute bottom-0 right-0">
-        <div className="flex flex-col items-end">
-          <div className="py-1">
-            <a href="#">
-              <div
-                className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
+                {/* side buttons */}
+                <div className="mt-[6vw] mx-4 mb-4 absolute bottom-0 right-0">
+                    <div onClick={rulesRule} className="flex flex-col items-end">
+                        <div className="py-1">
+                                <div
+                                    className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
                hover:w-[10.75rem] sm:hover:w-[14.25rem] md:hover:w-[14.25rem] lg:hover:w-[14.25rem] xl:hover:w-[14.25rem] hover:bg-[#000000] hover:border-2 hover:border-[#00C29A] transition-all duration-300 text-[#00C29A]"
-              >
-                <Image
-                  src={"/images/rulebook.png"}
-                  width={100}
-                  height={100}
-                  alt="Rulebook"
-                  loading="eager"
-                  className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10 "
-                ></Image>
-                <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
-                  RULEBOOK
-                </span>
-              </div>
-            </a>
-          </div>
+                                >
+                                    <Image
+                                        src={"/images/rulebook.png"}
+                                        width={100}
+                                        height={100}
+                                        alt="Rulebook"
+                                        loading="eager"
+                                        className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10 "
+                                    ></Image>
+                                    <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
+                                        RULEBOOK
+                                    </span>
+                                </div>
+                        </div>
 
-          <div className="py-1">
-            <Link href="/leaderboard">
-              <div
-                className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
+                        <div className="py-1">
+                            <Link href="/leaderboard">
+                                <div
+                                    className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
               hover:w-[12.5rem] sm:hover:w-[16.75rem] md:hover:w-[16.75rem] lg:hover:w-[16.75rem] xl:hover:w-[16.75rem]  hover:bg-[#000000] hover:border-2 hover:border-[#00C29A] transition-all duration-300 text-[#00C29A]"
-              >
-                <Image
-                  src={"/images/trophy.png"}
-                  width={100}
-                  height={100}
-                  alt="Rulebook"
-                  loading="eager"
-                  className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10"
-                ></Image>
-                <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
-                  LEADERBOARD
-                </span>
-              </div>
-            </Link>
-          </div>
+                                >
+                                    <Image
+                                        src={"/images/trophy.png"}
+                                        width={100}
+                                        height={100}
+                                        alt="LEADERBOARD"
+                                        loading="eager"
+                                        className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10"
+                                    ></Image>
+                                    <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
+                                        LEADERBOARD
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
 
-          <div className="py-1">
-            <a href="#">
-              <div
-                className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
+                        <div className="py-1">
+                            <a href="https://matias.ma/nsfw" target="blank">
+                                <div
+                                    className=" flex items-center overflow-hidden bg-[#00C29A] p-3 sm:p-3 md:p-3 lg:p-3 xl:p-3 w-10 sm:w-16 md:w-16 lg:w-16 xl:w-16 h-10 sm:h-16 md:h-16 lg:h-16 xl:h-16
               hover:w-[9.75rem] sm:hover:w-[12.9rem] md:hover:w-[12.9rem] lg:hover:w-[12.9rem] xl:hover:w-[12.9rem]  hover:bg-[#000000] hover:border-2 hover:border-[#00C29A] transition-all duration-300 text-[#00C29A]"
-              >
-                <Image
-                  src={"/images/monkey.png"}
-                  width={100}
-                  height={100}
-                  alt="Rulebook"
-                  loading="eager"
-                  className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10"
-                ></Image>
+                                >
+                                    <Image
+                                        src={"/images/monkey.png"}
+                                        width={100}
+                                        height={100}
+                                        alt="Monkeyyyy"
+                                        loading="eager"
+                                        className="w-6 sm:w-10 md:w-10 lg:w-10 xl:w-10"
+                                    ></Image>
 
-                <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
-                  MONKEY
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* side buttons end */}
-    </div>
-  );
+                                    <span className="pl-4 font-heading text-xs sm:text-lg md:text-lg lg:text-lg xl:text-lg">
+                                        MONKEY
+                                    </span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                {/* side buttons end */}
+            </div>
+            <Rulebook rules1={rules1} rulesRule={rulesRule} />
+        </>
+    );
 }
---
+++ frontend/src/components/Rulebook.jsx
@@ -0,0 +1,74 @@
+export const Rulebook = (props) => {
+    return (
+        <div
+            className={
+                "flex transition-all duration-1000 py-20 z-20 relative w-full h-fit bg-black bg-opacity-25 backdrop-blur-sm justify-center items-center" +
+                props.rules1
+            }
+        >
+            <div className="relative w-11/12 pb-20 bg-[#0B0B0B] flex flex-col justify-center items-center border-3 border-[#00C29A]">
+                <h1 className="flex justify-center items-center text-[#00C29A] text-center font-Autom text-5xl md:text-6xl m-20 mb-10">
+                    Rulebook
+                </h1>
+                <button
+                    onClick={props.rulesRule}
+                    className="absolute top-0 right-0 w-20 h-20 text-[#00C29A] text-5xl text-center font-Autom transition-all duration-300 hover:bg-main hover:text-black"
+                >
+                    X
+                </button>
+                <div className="mb-5 w-11/12 py-5 flex flex-col justify-center items-center bg-[#16161600]">
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            1.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            What does the fox say? Does the fox say? The fox
+                            say? Fox say? Say? ? What does the fox say? Does the
+                            fox say? The fox say? Fox say? Say? ?
+                        </div>
+                    </div>
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            2.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            Click Monkey in the Menu
+                        </div>
+                    </div>
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            3.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            I'm sexy and I know it.
+                        </div>
+                    </div>
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            4.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            Dance while reading rule no. 3
+                        </div>
+                    </div>
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            5.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            Lmfao
+                        </div>
+                    </div>
+                    <div className="mb-5 bg-[#181717] w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-1/12 pt-3 pl-3 m-5 mt-14 mr-3 flex justify-center items-center font-Minecraftia text-start text-4xl md:m-10 md:text-5xl">
+                            6.
+                        </div>
+                        <div className="mx-5 py-10 w-10/12 flex flex-col justify-center items-start text-center text-[#00C29A] font-fira-code-2 text-md md:text-left md:text-lg">
+                            Good Job
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
