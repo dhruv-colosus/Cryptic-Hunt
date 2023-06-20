0c8b33fcc0937e55e18865c0dc7be710354bbf7d
Dhruv <dhruvdeora31@gmail.com>
2023-06-20 17:08:22 +0530

    fixed mesage box

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Message_box from "@/components/message_box";
 import { useState } from "react";
 
 export default function Login() {
@@ -8,57 +9,60 @@ export default function Login() {
   const login = () => {};
 
   return (
-    <div className="flex justify-center items-center h-screen bg-background">
-      <div className="w-96 p-6">
-        <h1 className="sm:text-5xl text-3xl block text-center font-heading text-main">
-          Login
-        </h1>
-        <div className="sm:mt-12 mt-8">
-          <label className="block sm:text-xl text-base mb-2 font-heading">
-            Username
-          </label>
-          <input
-            type="text"
-            name="username"
-            className="sm:border-3 border-2 outline-none border-main w-full text-base 
+    <>
+      <div className="flex justify-center items-center h-screen bg-background">
+        <div className="w-96 p-6">
+          <h1 className="sm:text-5xl text-3xl block text-center font-heading text-main">
+            Login
+          </h1>
+          <div className="sm:mt-12 mt-8">
+            <label className="block sm:text-xl text-base mb-2 font-heading">
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="sm:border-3 border-2 outline-none border-main w-full text-base 
                 px-2 py-2 font-sub bg-background"
-            placeholder="Enter Username..."
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="mt-7">
-          <label className="block sm:text-xl text-base mb-2 font-heading">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            className="sm:border-3 border-2 outline-none border-main w-full text-base 
+              placeholder="Enter Username..."
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mt-7">
+            <label className="block sm:text-xl text-base mb-2 font-heading">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="sm:border-3 border-2 outline-none border-main w-full text-base 
                 px-2 py-2 font-sub bg-background "
-            placeholder="Enter Password..."
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+              placeholder="Enter Password..."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
 
-        <div className="sm:mt-12 mt-10 flex justify-center items-center">
-          <button
-            type="submit"
-            className="block text-center outline-none bg-main text-black cursor-pointer transition  duration-300
+          <div className="sm:mt-12 mt-10 flex justify-center items-center">
+            <button
+              type="submit"
+              className="block text-center outline-none bg-main text-black cursor-pointer transition  duration-300
                 sm:py-2 py-1 sm:px-12 px-10 border-2 border-main font-button hover:bg-background hover:text-main
                  onClick={login}"
-          >
-            Login
-          </button>
-        </div>
-        <div className="mt-5 flex justify-center items-center text-center">
-          <p className="text-white font-sub">
-            Don’t have an account?{" "}
-            <a href="/register" className="underline">
-              SignUp
-            </a>
-          </p>
+            >
+              Login
+            </button>
+          </div>
+          <div className="mt-5 flex justify-center items-center text-center">
+            <p className="text-white font-sub">
+              Don’t have an account?{" "}
+              <a href="/register" className="underline">
+                SignUp
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <Message_box />
+    </>
   );
 }
