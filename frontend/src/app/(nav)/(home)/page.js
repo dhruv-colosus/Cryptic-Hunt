85a69302752706a0d597d0a6ffb0e09a318aea97
Dhruv <58906323+dhruv-colosus@users.noreply.github.com>
2023-06-22 23:19:11 +0530

    Revert "Misc"

--- frontend/src/app/(nav)/(home)/page.js
+++ frontend/src/app/(nav)/(home)/page.js
@@ -1,63 +1,50 @@
-"use client";
-
-import MessageBox from "@/components/message-box";
 import { PlayButton } from "@/components/play-btn";
 import Side_button from "@/components/side_button";
-// import { useRef, useEffect, useState } from "react";
-import { useAtom } from "jotai";
-import { errorAtom } from "@/atoms/error";
 
 export default function Home() {
+  return (
+    <>
+      {/* body */}
 
-    const [error, setError] = useAtom(errorAtom);
-    const showMessage=()=>{setError({title:"LOL", message:"lolololololol"});  setTimeout(hideMessage, 10000)}
-    async function hideMessage(){setError(null)}
-    return (
-        <>
-            <MessageBox error={error} hideMessage={hideMessage} showMessage={showMessage} />
-            {/* body */}
-            <div className="fixed w-full top-0 bg-[#OBOBOB] h-fit py-[30vh] sm:pt-44 md:pt-44 lg:pt-44 xl:pt-44 transition-all duration-300">
-                <div className="flex flex-col items-center">
-                    {/* main heading */}
-                    <div>
-                        <div>
-                            <h1 className="text-[#00C29A] font-heading text-5xl sm:text-7xl md:text-7xl lg:text-7xl xl:text-8xl transition-all duration-300">
-                                CRYPTIC HUNT
-                            </h1>
-                        </div>
-                    </div>
-                    {/* main heading ends */}
-
-                    {/* under heading */}
-                    <div className="pb-4">
-                        <div>
-                            <h3
-                                className="text-[#00C29A] font-montserrat tracking-[0.75rem] sm:tracking-[1.75rem] md:tracking-[1.75rem] lg:tracking-[1.75rem] xl:tracking-[1.75rem]
-               text-[0.4rem] sm:text-xs md:text-xs lg:text-xs xl:text-xs transition-all duration-300"
-                            >
-                                LET THE HUNT BEGIN!
-                            </h3>
-                        </div>
-                    </div>
-                    {/* under heading ends */}
-
-                    {/* play button */}
-                    <div className="">
-                        <PlayButton />
-                    </div>
-                    {/* play button ends */}
-                </div>
-                <button className="border border-white" onClick={showMessage}>
-                    Top Right Corner Message Button
-                </button>
+      <div className="sticky top-32 bg-[#OBOBOB] h-auto py-[30vh] sm:pt-44 md:pt-44 lg:pt-44 xl:pt-44 transition-all duration-300">
+        <div className="flex flex-col items-center">
+          {/* main heading */}
+          <div>
+            <div>
+              <h1 className="text-[#00C29A] font-heading text-5xl sm:text-7xl md:text-7xl lg:text-7xl xl:text-8xl transition-all duration-300">
+                CRYPTIC HUNT
+              </h1>
             </div>
-            {/* body ends */}
+          </div>
+          {/* main heading ends */}
 
-            {/* side buttons */}
+          {/* under heading */}
+          <div className="pb-4">
+            <div>
+              <h3
+                className="text-[#00C29A] font-montserrat tracking-[0.75rem] sm:tracking-[1.75rem] md:tracking-[1.75rem] lg:tracking-[1.75rem] xl:tracking-[1.75rem]
+               text-[0.4rem] sm:text-xs md:text-xs lg:text-xs xl:text-xs transition-all duration-300"
+              >
+                LET THE HUNT BEGIN!
+              </h3>
+            </div>
+          </div>
+          {/* under heading ends */}
 
-            <Side_button />
+          {/* play button */}
+          <div className="">
+            <PlayButton />
+          </div>
+          {/* play button ends */}
+        </div>
+      </div>
+      {/* body ends */}
 
-            {/* side buttons end */}
-        </>
-    );
+      {/* side buttons */}
+
+      <Side_button />
+
+      {/* side buttons end */}
+    </>
+  );
 }
